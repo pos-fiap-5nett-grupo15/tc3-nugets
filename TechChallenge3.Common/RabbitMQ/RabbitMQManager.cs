eb97c3cfd6241f2390de589e3465d38a0444d598
luisgustavoreimberg <luisgustavoreimberg@gmail.com>
2024-12-17 23:36:39 -0300

    Export from AzureDevOps

---
+++ TechChallenge3.Common/RabbitMQ/RabbitMQManager.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace TechChallenge3.Common.RabbitMQ
+{
+    public class RabbitMQManager
+    {
+        public static async Task PublishAsync(
+            object message,
+            string hostName,
+            int port,
+            string userName,
+            string password,
+            string exchangeName,
+            string routingKeyName,
+            CancellationToken ct)
+        {
+            // Criar uma conexão com o RabbitMQ
+            var factory = new ConnectionFactory()
+            {
+                HostName = hostName,
+                Port = port,
+                UserName = userName,
+                Password = password
+            };
+            using (var connection = await factory.CreateConnectionAsync())
+            using (var channel = await connection.CreateChannelAsync())
+            {
+                // Converter a mensagem para bytes
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+                // Enviar a mensagem para a fila
+                await channel.BasicPublishAsync(exchangeName, routingKeyName, body, ct);
+            }
+        }
+    }
+}
---
+++ TechChallenge3.Infrastructure/Settings/JwtSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace TechChallenge3.Infrastructure.Settings
+{
+    public class JwtSettings
+    {
+        private const int DEFAULT_TOKEN_EXPIRES_MINUTES = 5;
+
+        public JwtSettings(int tokenExpiresInMinutes = DEFAULT_TOKEN_EXPIRES_MINUTES) =>
+            TokenExpiresInMinutes = tokenExpiresInMinutes;
+
+        public JwtSettings() { }
+
+        public string SecretKey { get; set; }
+        public int TokenExpiresInMinutes { get; set; }
+    }
+}
